ca7e652e7fd522780f97b2278a6d5286dddc4753
Chelsea Stats <ChelseaStats@users.noreply.github.com>
2017-02-17 09:29:03 +0000

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ if ('serviceWorker' in navigator) {
 
     // Register our service worker file. It has to be an external file for
     // certain lifecycles to occure.
-    navigator.serviceWorker.register('sw.js')
+    navigator.serviceWorker.register('https://www.thechels.uk/ServiceWorker/sw.js')
 
         // These promises aren't required but provide dev feedback.
         .then((registration) => {
