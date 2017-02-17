a397bbb12ba4492dc892e078f7bfcf22525e82e2
ChelseaStats <chelseastats@users.noreply.github.com>
2017-02-17 21:59:05 +0000

    error added

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ if ('serviceWorker' in navigator) {
 
     // Register our service worker file. It has to be an external file for
     // certain lifecycles to occure.
-    navigator.serviceWorker.register('sw.js')
+    navigator.serviceWorker
+    .register('sw.js')
+    .catch(err => console.error('There is a problem registering the service worker', err));
+    
 
         // These promises aren't required but provide dev feedback.
         .then((registration) => {
