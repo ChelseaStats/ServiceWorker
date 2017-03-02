4f3bf486835dfcfd7d22752c6129dbcb28bcebeb
Chelsea Stats <ChelseaStats@users.noreply.github.com>
2017-03-02 09:00:00 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -134,6 +134,18 @@ self.addEventListener('fetch', function(event) {
 
 
 
-self.addEventListener('activate', event => {
-    event.waitUntil(clearCache());
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          // Return true if you want to remove this cache,
+          // but remember that caches are shared across
+          // the whole origin
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
 });
