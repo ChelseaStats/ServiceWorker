4035e56beb7ab1cdbadc1291f0a20c06994607e3
Chelsea Stats <ChelseaStats@users.noreply.github.com>
2017-03-02 09:03:30 +0000

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ if ('serviceWorker' in navigator) {
     // certain lifecycles to occure.
     navigator.serviceWorker
     .register('sw.js')
-    .catch(err => console.error('There is a problem registering the service worker', err));
+    .catch(err => console.error('There is a problem registering the service worker', err))
     
         // These promises aren't required but provide dev feedback.
         .then((registration) => {
